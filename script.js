82137a5c996b879b6f3d16f0e43498aded7e8b98
TylerFleming <zaosyn@gmail.com>
2021-06-13 04:36:51 -0500

    nav bar wip

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-async function grabPosts(url) {
-    let data = await fetch(url);
-     data = await data.json();
-    let modifiedPost = data.map( post => post.content.rendered )
-    modifiedPost.forEach(post => {
-        let postDiv = document.createElement('div');
-        postDiv.style.display = 'block'
-        postDiv.innerHTML = post;
-        document.body.append(postDiv)
-    });
-}
+// async function grabPosts(url) {
+//     let data = await fetch(url);
+//      data = await data.json();
+//     let modifiedPost = data.map( post => post.content.rendered )
+//     modifiedPost.forEach(post => {
+//         let postDiv = document.createElement('div');
+//         postDiv.style.display = 'block'
+//         postDiv.innerHTML = post;
+//         document.body.append(postDiv)
+//     });
+// }
 
-grabPosts('https://doggiedayplay.000webhostapp.com/wp-json/wp/v2/posts')
+// grabPosts('https://doggiedayplay.000webhostapp.com/wp-json/wp/v2/posts')
 
 async function grabNav(url) {
+    const navList = document.querySelector('.navigation__list')
     let data = await fetch(url)
     navItems = await data.json()
     navItems = navItems.items
     navItems.forEach(navItem => {
+        const linkElementParent = document.createElement('li')
+        linkElementParent.classList.add('navigation__item')
         let linkText = navItem.title;
         let linkURL = navItem.url;
         let link = document.createElement('a');
         link.innerText = linkText;
         link.setAttribute('href', linkURL)
         link.setAttribute('target', '_blank')
-        document.body.append(link)
+        link.classList.add('navigation__link')
+        linkElementParent.append(link)
+        navList.append(linkElementParent)
     });
 }
 grabNav('https://doggiedayplay.000webhostapp.com/wp-json/menus/v1/menus/primary')
